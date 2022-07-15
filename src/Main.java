f0e81957df5c64ccd4687926743ae89958d3f5b0
Алексей Макаренко <alexchb.makarenko@yandex.ru>
2022-07-15 12:08:20 +0300

    name chargee

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
 
-        int k = 100;
-        int p = 1100;
-        int r = 100;
-        int bonus = p/r;
-        if (p>1000) {
+        int account = 100;
+        int put = 1100;
+        int one = 100;
+        int bonus = put / one;
+        if (put > 1000) {
             System.out.println("Количество бонусных рублей:" + bonus);
-            System.out.println("Итоговый счёт:" + (bonus+k+p));
-        }else {
+            System.out.println("Итоговый счёт:" + (bonus + account + put));
+        } else {
             System.out.println("Количество бонусных рублей:0");
-            System.out.println("Итоговый счёт:" + (k+p));
+            System.out.println("Итоговый счёт:" + (account + put));
         }
     }
 }
